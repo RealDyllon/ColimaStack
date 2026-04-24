0f47e6fe1b328e86c7090b73ea06d912776799be
dyllon <dyllon.g@outlook.com>
2026-04-25 04:06:52 +0800

    Initial commit

---
+++ ColimaStack/ColimaStack/ColimaStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColimaStackApp.swift
+//  ColimaStack
+//
+//  Created by Dyllon on 25/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ColimaStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
